f7db5dace94af9a67678cad988eb3a7f7042219f
panikersha <sergeevna.khm@gmail.com>
2019-08-20 12:42:04 +0300

    Create stepik_log_without_cmath.cpp

---
+++ stepik_log_without_cmath.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int log(int b, int count)
+{
+    int count_t = count;
+    if (b/2 == 0) return count_t;
+    else 
+    {
+        count_t = log(b/2, count_t+1);
+        return count_t;
+    } 
+}
+
+
+int main()
+{
+   int num = 0;
+   int a = 0;
+   int res = 0 ;
+
+   cin>>num;
+
+   for(int i=0;i<num;i++)
+   {
+       cin>>a;
+       res = log(a,0);
+       cout<<res<<endl;
+   }
+
+	return 0;
+}
