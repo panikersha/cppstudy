41c09500553c471309e93d1ae46ccf1c5c491557
panikersha <sergeevna.khm@gmail.com>
2019-08-23 00:27:00 +0300

    Update Dynamic_memory.cpp

--- Dynamic_memory.cpp
+++ Dynamic_memory.cpp
@@ -19,6 +19,32 @@ char *resize(const char *str, unsigned size, unsigned new_size)
 	return new_m;
 }
 
+char *getline()
+{
+	char ch = '\0';
+	int size = 1;
+	char * row = new char[size];
+	*row = ch;
+	
+	while (cin.get(ch) && ch != '\0' && ch != cin.eof() && ch != '\n')
+	{
+		
+		char * temp = new char[size+1];
+		int i = 0;
+		for (i = 0; i < size-1; i++)
+		{
+			temp[i] = row[i];
+		}
+		size++;
+		temp[i] = ch;
+		temp[i + 1] = '\0';
+		delete [] row;
+		row = 0;
+		row = temp;
+	}
+	return row;
+}
+
 int main()
 {
 	char *s = new char[sizeof("Stepik.org")]{ "Stepik.org" };
@@ -28,5 +54,13 @@ int main()
 	cout << new_S;
 	delete [] new_S;
 	new_S = 0;
+	
+	
+	char * row;
+	row = getline(); 
+	cout << row << endl;
+	delete [] row;
+	row = 0;
+	
 	return 0;
 }
