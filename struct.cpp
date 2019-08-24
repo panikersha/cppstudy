bf5177ba5bf5e9b600526698b5d2120c03352fba
panikersha <sergeevna.khm@gmail.com>
2019-08-24 23:21:26 +0300

    Create struct.cpp

---
+++ struct.cpp
@@ -0,0 +1,32 @@
+#include "pch.h"
+#include <iostream>
+#include <cstddef> // size_t
+#include <cstring> // strlen, strcpy
+
+using namespace std;
+
+
+
+struct String {
+
+	/* Реализуйте этот конструктор */
+	String(const char *str = "")
+	{
+		size = strlen(str);
+		this->str = new char[size + 1];
+		strcpy(this->str, str);
+
+	}
+
+	size_t size;
+	char *str;
+};
+
+
+int main()
+{
+	String p ("Hello\0");
+	cout << p.size << " " << p.str;
+
+	return 0;
+}
