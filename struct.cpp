0f49e1d32b5861b7ac04b0087e5e1cb47e26f88f
panikersha <sergeevna.khm@gmail.com>
2019-08-24 23:47:33 +0300

    Update struct.cpp

--- struct.cpp
+++ struct.cpp
@@ -16,7 +16,18 @@ struct String {
 		strcpy(this->str, str);
 
 	}
-
+	
+	void append(String &other)
+	{
+		size_t size_other = strlen(other.str);
+		char *str_ap = new char[this->size + other.size+1];
+		strcpy_s(str_ap,this->size+1, this->str);
+		strcpy_s(str_ap+ size, other.size+1,other.str);
+		delete [] this->str;
+		this->str = str_ap;
+		this->size += other.size ;
+	}
+	
 	size_t size;
 	char *str;
 };
